9a5ae616eb8cb3f87026d2e65ae9770f218b38dc
Hemanth23 <116500500+hemanthv23@users.noreply.github.com>
2024-11-05 11:30:06 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: "Email is needed" },
 });
 
 module.exports = mongoose.model("User", userSchema);
